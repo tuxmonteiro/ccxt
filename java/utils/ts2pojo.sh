9383d8f312d807b6cc087cb6b6ad3fa5e5977f17
tuxmonteiro <me@tuxmonteiro.dev>
2025-04-27 22:46:47 +0200

    java/utils: update ts2pojo

--- java/utils/ts2pojo.sh
+++ java/utils/ts2pojo.sh
@@ -1,23 +1,25 @@
 #!env bash
 
-JSON_DIR="../../../../../../resources/json-schema/"
+JSON_DIR="./src/main/resources/json-schema/"
+JAVA_DIR="./src/main/java/dev/tuxmonteiro/jccxt/base/types/"
+TYPES_TS="../ts/src/base/types.ts"
 
 # typescript to json-schema
 grep 'export interface' types.ts | grep -v Dictionary | awk '{ print $3 }' | while read class; do
   echo $class
   # npm install --save ts-json-schema-generator
-  npx ts-json-schema-generator --path types.ts --type $class > $JSON_DIR/${class%Interface}.json
+  npx ts-json-schema-generator --path ${TYPES_TS} --type $class > $JSON_DIR/${class%Interface}.json
   sed -i 's/Interface//g' $JSON_DIR/${class%Interface}.json
 done
 
 ## TODO: Automatic fix DepositWithdrawFee.json (networks)
 
 # json-schema to Java POJO
-ls -1 ../../../../../../resources/json-schema/ | sed 's/.json//' | while read class; do
+ls -1 ${JSON_DIR} | sed 's/.json//' | while read class; do
 
   # https://github.com/joelittlejohn/jsonschema2pojo/releases
-  echo -e 'package dev.tuxmonteiro.jccxt.base.types;\n' > $class.java
-  ./jsonschema2pojo-1.2.2/bin/jsonschema2pojo -o -sl -s $JSON_DIR/$class.json -t generated -log trace
-  [ -f generated/$class.java ] && cat generated/$class.java >> $class.java || rm -f $class.java
+  echo -e 'package dev.tuxmonteiro.jccxt.base.types;\n' > ${JAVA_DIR}/$class.java
+  ./jsonschema2pojo-1.2.2/bin/jsonschema2pojo -o -sl -s ${JSON_DIR}/$class.json -t ${JSON_DIR} -log trace
+  [ -f ${JSON_DIR}/$class.java ] && cat ${JSON_DIR}/$class.java >> ${JAVA_DIR}/$class.java || rm -vf ${JSON_DIR}/$class.java ${JAVA_DIR}/$class.java
   echo
 done
