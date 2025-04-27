385ed23e9ef071ad81556be537de79936599c171
tuxmonteiro <me@tuxmonteiro.dev>
2025-04-27 23:26:20 +0200

    java/utils: fix ts2pojo

--- java/utils/ts2pojo.sh
+++ java/utils/ts2pojo.sh
@@ -4,13 +4,13 @@ JSON_DIR="./src/main/resources/json-schema/"
 JAVA_DIR="./src/main/java/dev/tuxmonteiro/jccxt/base/types/"
 TYPES_TS="../ts/src/base/types.ts"
 
-rm -f ${JSON_PATH}/*.json
+rm -f ${JSON_PATH}/*.{json,java}
 
 # typescript to json-schema
 grep 'export interface' ${TYPES_TS} | grep -v Dictionary | awk '{ print $3 }' | while read class; do
   echo $class
-  # npm install --save ts-json-schema-generator
-  npx ts-json-schema-generator --path ${TYPES_TS} --type $class > $JSON_DIR/${class%Interface}.json
+  npm install --save ts-json-schema-generator
+#npx ts-json-schema-generator --path ${TYPES_TS} --type $class > $JSON_DIR/${class%Interface}.json
   sed -i 's/Interface//g' $JSON_DIR/${class%Interface}.json
 done
 
@@ -21,9 +21,13 @@ sed -i 's|"$ref": "#/definitions/Dictionary%3CDepositWithdrawFeeNetwork%3E"|"typ
 ls -1 ${JSON_DIR} | grep '.json' | sed 's/.json//' | while read class; do
 
   # https://github.com/joelittlejohn/jsonschema2pojo/releases
-  echo -e 'package dev.tuxmonteiro.jccxt.base.types;\n' > ${JAVA_DIR}/$class.java
   ./utils/jsonschema2pojo-1.2.2/bin/jsonschema2pojo -o -sl -s ${JSON_DIR}/$class.json -t ${JSON_DIR} -log trace
-  [ -f ${JSON_DIR}/$class.java ] && cat ${JSON_DIR}/$class.java >> ${JAVA_DIR}/$class.java || rm -vf ${JAVA_DIR}/$class.java
-  rm -f ${JSON_DIR}/$class.java 
+  echo
+done
+
+ls -a ${JSON_DIR} | grep '.java' | sed 's/.java//' | while read class; do
+  echo -e 'package dev.tuxmonteiro.jccxt.base.types;\n' | tee ${JAVA_DIR}/$class.java
+  cat ${JSON_DIR}/$class.java | tee -a ${JAVA_DIR}/$class.java
+  rm -f ${JSON_DIR}/$class.java
   echo
 done
